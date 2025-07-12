0d9c83200aeb1f84412df5c35130f49c2dee1c84
adhitht <hello@adhitht.me>
2025-07-12 21:16:22 +0530

    fix: contact details in about page

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -1,7 +1,7 @@
 export const contactDetails = {
-  teamCaptain: 'Ashish Sam',
+  teamCaptain: 'Vinay Kumar Maddila',
   email: 'saebaja@iith.ac.in',
-  phone: '(+91) 040 2301 6055',
+  phone: '+91 80089 87377',
   address: 'Indian Institute of Technology Hyderabad, Kandi, Sangareddy, Telangana - 502285',
   mapEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d356.1345283645608!2d78.1255228559298!3d17.59985949143472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbf70007430951%3A0x2a6f9b34a59295d3!2sIITH%20Aeolus%20Racing%20Workshop!5e0!3m2!1sen!2sin!4v1752219605199!5m2!1sen!2sin',
 };
